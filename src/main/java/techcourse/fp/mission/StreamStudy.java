cf8b6d26e63e882cf01bc0df1681bf6705ce32d7
daeun <leede0418@likelion.org>
2021-03-16 11:52:03 +0900

    feat(stream-study): sumOverThreeAndDouble() 구현

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -34,7 +34,10 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToInt(number -> number * 2)
+                .reduce(0, Integer::sum);
     }
 
     public static void printLongestWordTop100() throws IOException {
